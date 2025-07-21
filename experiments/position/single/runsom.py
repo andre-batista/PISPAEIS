01267b902d3c3b03ff97909073263a1d1680b328
André Batista <andre-costa@ufmg.br>
2025-07-21 17:21:23 -0300

    Finish study with high contrast star

--- experiments/position/single/runsom.py
+++ experiments/position/single/runsom.py
@@ -9,8 +9,7 @@ import richmond as ric
 file_path = '../../../data/position/single/'
 file_name = 'star4.ipt'
 
-test = ipt.InputData.importdata(import_filename=file_name,
-                                import_filepath=file_path)
+test = ipt.InputData(import_filename=file_name, import_filepath=file_path)
 
 resolution = (40, 40)
 
@@ -18,7 +17,7 @@ discretization = ric.Richmond(configuration=test.configuration,
                               elements=resolution)
 
 method = som.SubspaceBasedOptimizationMethod(
-    stp.StopCriteria(max_iterations=600), cutoff_index=15
+    stp.StopCriteria(max_iterations=1000), cutoff_index=15
 )
 
 result = method.solve(test, discretization, print_info=True)
