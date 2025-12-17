e419862340b483fcf5619d5e50261303da4f5673
unknown <wmwandre@gmail.com>
2025-12-17 10:36:50 -0300

    Preparing experiment for indicator comparison considering position error

---
+++ experiments/indicatorcomparison/position/gendata.py
@@ -0,0 +1,52 @@
+import sys
+sys.path.insert(1, '../../../eispy2d/library/')
+
+import configuration as cfg
+import inputdata as ipt
+import result as rst
+import draw
+import mom_cg_fft as mom
+
+# Parameters
+name = 'star'
+file_path = '../../../data/indicatorcomparison/position/'
+NM = NS = 80
+Ro = 4.
+lambda_b = 1.
+epsilon_rb = 1.
+Lx = Ly = 2.
+E0 = 1.
+perfect_dielectric = True
+resolution = (120, 120)
+noise = 5.
+indicators = [rst.SHAPE_ERROR, rst.POSITION_ERROR,
+              rst.REL_PERMITTIVITY_PAD_ERROR, rst.SSIM_ERROR]
+epsilon_rd = 6.
+l = 0.6
+position = [.35, .35]
+
+config = cfg.Configuration(name=name + '.cfg', number_measurements=NM, 
+                           number_sources=NS, observation_radius=Ro, 
+                           wavelength=lambda_b,
+                           background_permittivity=epsilon_rb,
+                           image_size=[Ly, Lx], magnitude=E0,
+                           perfect_dielectric=perfect_dielectric)
+
+
+test = ipt.InputData(name=name + '.ipt', configuration=config, 
+                     resolution=resolution, noise=noise, indicators=indicators)
+
+test.rel_permittivity, _ = draw.star5(
+    l, axis_length_x=Lx, axis_length_y=Ly, resolution=resolution,
+    background_rel_permittivity=epsilon_rb, object_rel_permittivity=epsilon_rd,
+    center=position, rotate=0.
+)
+
+forward = mom.MoM_CG_FFT(tolerance=1e-3, maximum_iterations=10_000,
+                         parallelization=True)
+
+_ = forward.solve(test)
+
+test.compute_dnl()
+
+test.save(file_path=file_path)
